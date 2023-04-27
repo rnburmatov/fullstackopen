b0a2fea7896b24b07393f87a25d5c533dc2cb6dc
rnburmatov <rnburmatov@gmail.com>
2023-04-27 15:23:42 +0300

    add notes submit and notes filter

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -3,13 +3,40 @@ import Note from './components/Note';
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('A new note');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  }
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value);
+  }
   
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>{showAll ? 'Important' : 'Show All'}</button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} content={note.content}/>)}
+        {notesToShow.map(note => <Note key={note.id} content={note.content}/>)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   )
 }
