b18d26a06105db0ef9cce1062f0b1ae03e6089f1
rnburmatov <rnburmatov@gmail.com>
2023-04-28 10:12:34 +0300

    add exercise 2.11

--- part-2/exercise-2.6-phonebook/src/App.jsx
+++ part-2/exercise-2.6-phonebook/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 import Filter from './components/Filter';
 import Form from './components/Form';
 import List from './components/List';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const promise = axios.get('http://localhost:3001/persons').then(response => setPersons(response.data))
+  }, [])
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
